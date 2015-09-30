07688cdf1bcaf08ede30cfbdd23b6dab62f746e7
Kyle Wagner <tskazion@gmail.com>
2015-09-30 18:48:42 +0000

    Adds single page view for series.

--- series/views.py
+++ series/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from series.models import SeriesPost
+from django.shortcuts import get_object_or_404
 
 def series_episodes_view(request, sSlug):
-    pass
-
-def series_episode_single_view(request, eSlug):
 	pass
+
+def series_episode_single_view(request, sSlug, eSlug):
+	
+	episode = get_object_or_404(SeriesPost, series__slug=sSlug, slug=eSlug)
+	
+	return render(  request, 
+					episode.single_template, 
+					{
+						'episode': episode, 
+					})
